bf02bb687d1d5c0b37876375ee82fa0a0d77304f
joao pedro <joaopedrot1912@gmail.com>
2025-03-02 18:14:07 -0300

    change

---
+++ k8s/install_nginx_ingress_controller.sh
@@ -0,0 +1,6 @@
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
+helm upgrade --install ingress-nginx ingress-nginx \ 
+             --repo https://kubernetes.github.io/ingress-nginx \ 
+             --namespace ingress-nginx \
+             --create-namespace
