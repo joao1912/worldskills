9795dda5b745bb7386fa1db2a0199f0649a5605d
Joao Pedro <joaopedrot1912@gmail.com>
2025-03-29 17:43:55 -0300

    feat: app2container instalation

---
+++ utils/install_app2container_linux.sh
@@ -0,0 +1,13 @@
+# install bin
+
+curl -o AWSApp2Container-installer-linux.tar.gz https://app2container-release-us-east-1.s3.us-east-1.amazonaws.com/latest/linux/AWSApp2Container-installer-linux.tar.gz
+
+# extract
+
+sudo tar xvf AWSApp2Container-installer-linux.tar.gz
+
+# run the install
+
+sudo ./install.sh
+
+# init the A2C: sudo app2container init
---
+++ utils/install_app2container_wind.sh
@@ -0,0 +1,9 @@
+# install bin
+
+https://app2container-release-us-east-1.s3.us-east-1.amazonaws.com/latest/windows/AWSApp2Container-installer-windows.zip
+
+# run the install
+
+.\install.ps1
+
+# init the A2C: app2container init
