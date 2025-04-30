5b07a82148e01029596021dce8b64066c66c5245
Joao Pedro <joaopedrot1912@gmail.com>
2025-04-30 15:22:37 -0300

    feat: postgres client

---
+++ amazon_linux_2/install_postgres_client.sh
@@ -0,0 +1,5 @@
+sudo amazon-linux-extras install postgresql10
+
+# Exemplo de uso
+
+psql -h host.com -U usuario -p 5432 -d database_name
---
+++ amazon_linux_2023/install_postgres_client.sh
@@ -0,0 +1,7 @@
+sudo dnf update
+
+sudo dnf install postgresql15
+
+# Exemplo de uso
+
+psql -h host.com -U usuario -p 5432 -d database_name
