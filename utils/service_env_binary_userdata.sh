e6eb099a68efbb590465d97ca94856fa1f6fb5b1
Joao Pedro <joaopedrot1912@gmail.com>
2025-05-07 17:04:59 -0300

    changes

--- utils/service_env_binary_userdata.sh
+++ utils/service_env_binary_userdata.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+WORKPATH="/home/ec2-user"
+
+# Nome do arquivo de conf
+CONFIG_FILE_NAME="server.ini"
+
+# Bucket onde está o binario
 BUCKET_NAME="name"
+
+# Caminho de onde esta o binario
 BINARY_PATH="binario/bin"
+
 BINARY_NAME=$(basename "$BINARY_PATH")
 
-aws s3 cp s3://$BUCKET_NAME/$BINARY_PATH /home/ec2-user/
+aws s3 cp s3://$BUCKET_NAME/$BINARY_PATH $WORKPATH/
 
-chmod +x /home/ec2-user/$BINARY_NAME
+chmod +x $WORKPATH/$BINARY_NAME
+
+touch $WORKPATH/$CONFIG_FILE_NAME
+
+# DEFINIR ENVS AQUI
+
+echo '"MemcacheHost"="host"' >> $WORKPATH/server.ini
+echo '"MemcachePort"=0000' >> $WORKPATH/server.ini
+echo '"LogsPath"="./"' >> $WORKPATH/server.ini
 
 cat > /etc/systemd/system/ws-server.service << EOF
 [Unit]
